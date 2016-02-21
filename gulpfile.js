ff77e057446ce85001822fe46f96c0b31638666a
Mikael Jagekrans <micke.jagekrans@gmail.com>
2016-02-21 23:58:45 +0100

    fixed gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
+var es = require('event-stream');
 var del = require('del');
- 
-gulp.task('sass', function (done) {
-	gulp.src('./src/sass/**/*.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('./www/css'), done);
+
+gulp.task('clean', function (done) {
+	return del('www/**/*');
 });
 
-gulp.task('js', function (done) {
+gulp.task('js', ['clean'], function (done) {
 	var jsfiles = [
 		'./src/lib/jquery/dist/jquery.min.js',
 		'./src/lib/bootstrap-sass/assets/javascripts/bootstrap.min.js',
@@ -27,27 +26,32 @@ gulp.task('js', function (done) {
 	];
 
 	gulp.src(jsfiles)
-      	.pipe(concat('app.min.js'))
-      	.pipe(gulp.dest('./www/js'), done);
+		.pipe(concat('app.min.js'))
+  	.pipe(gulp.dest('./www/js'), done);
+
 });
 
-gulp.task('clean', function (done) {
-	return del('www/**/*');
+gulp.task('sass', ['js'], function (done) {
+	gulp.src('./src/sass/**/*.scss')
+		.pipe(sass.sync().on('error', sass.logError))
+		.pipe(gulp.dest('./www/css'), done);
 });
 
-gulp.task('copy', function (done) {
-	gulp.src(['./src/*.html', './src/js/**/*.html'])
-		.pipe(gulp.dest('./www'));
-	gulp.src(['./src/fonts/*', './src/lib/font-awesome/fonts/*'])
-		.pipe(gulp.dest('./www/fonts'));
-	gulp.src('./src/img/*')
-		.pipe(gulp.dest('./www/img'), done)
+gulp.task('copy', ['clean', 'sass', 'js'], function (done) {
+	es.concat(
+		gulp.src(['./src/*.html', './src/js/**/*.html'])
+			.pipe(gulp.dest('./www')),
+		gulp.src(['./src/fonts/*', './src/lib/font-awesome/fonts/*'])
+			.pipe(gulp.dest('./www/fonts')),
+		gulp.src('./src/img/*')
+			.pipe(gulp.dest('./www/img'))
+	).on('end', done);
 });
- 
+
 gulp.task('watch', function () {
 	gulp.watch('./src/sass/**/*.scss', ['sass']);
 	gulp.watch('./src/js/**/*.js', ['js']);
 	gulp.watch(['./src/*.html', './src/js/**/*.html', './src/fonts/*', './src/img/*'], ['copy']);
 });
 
-gulp.task('default', ['clean', 'sass', 'js', 'copy']);
+gulp.task('default', ['copy']);
